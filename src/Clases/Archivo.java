be4b9ff84ccd2f223d2dd1c145343b68ce85ac84
Chuchoncio <jmpch.111@gmail.com>
2024-05-25 19:01:30 -0300

    Arreglando problemas

--- src/Clases/Archivo.java
+++ src/Clases/Archivo.java
@@ -6,31 +6,6 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Archivo {
-    // Abro el archivo de entrada
-/*/    private BufferedReader reader;
-    private StringTokenizer tokenizer;
-
-    public Archivo(String nombreArchivo) throws IOException {
-        this.reader = new BufferedReader(new FileReader(nombreArchivo));
-        this.tokenizer = new StringTokenizer(reader.readLine());
-    }
-
-    public BufferedReader getReader() {
-        return reader;
-    }
-
-    public void setReader(BufferedReader reader) {
-        this.reader = reader;
-    }
-
-    public StringTokenizer getTokenizer() {
-        return tokenizer;
-    }
-
-    public void setTokenizer(StringTokenizer tokenizer) {
-        this.tokenizer = tokenizer;
-    }*/
-
     private BufferedReader reader;
     private StringTokenizer tokenizer;
 
@@ -38,6 +13,9 @@ public class Archivo {
         this.reader = new BufferedReader(new FileReader(nombreArchivo));
         this.tokenizer = new StringTokenizer(reader.readLine());
     }
+
+    //TO-DO: Abrir archivo de escritura (o agregar otro constructor)
+    //To-DO2: Escribir en archivo
 
     public int leerEntero() throws IOException {
         if (!tokenizer.hasMoreTokens()) {
@@ -57,14 +35,13 @@ public class Archivo {
         return arreglo;
     }
 
-    public void cerrar() throws IOException {
-        reader.close();
-    }
-
     public void leerDatos(Aventurero aventurero) throws IOException {
         int cantidadOias = leerEntero();
         aventurero.setCantidadOias(cantidadOias);
         aventurero.setPasadas(leerArreglo(cantidadOias - 1));
     }
 
+    public void cerrar() throws IOException {
+        reader.close();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,46 +7,8 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static List<Integer> crearRondaOias(int oias) {
-        List<Integer> rondaOias = new LinkedList<>();
-        for (int i = 1; i <= oias; i++) {
-            rondaOias.add(i);
-        }
-        return rondaOias;
-    }
-
-    public static int identificadorCebador(String output,int oias,int[] pass,List<Integer> rondaOias)
-    {
-        PrintWriter pw = null;
-        try{
-            pw = new PrintWriter(new FileWriter(output));
-            int position = 0;
-            for (int i = 0; i < oias - 1; i++) {
-                position = (position + pass[i]) % rondaOias.size();
-                int removed = rondaOias.remove(position);
-                //escribirOia(pw,removed)
-                pw.print(removed);	//escribirOia
-                pw.print(" ");		//escribirOia
-            }
-            int removed = rondaOias.remove(0);
-            //escribirCebador(pw,removed)
-            pw.println();			//escribirCebador
-            pw.println(removed);	//escribirCebador
-            
-            return removed;		
-        }catch(IOException e) {
-            e.printStackTrace();
-            return -1;
-        }finally {
-            if(pw != null){
-                pw.close();
-            }
-        }
-    }
-
     public static void main(String[] args) throws IOException {
-        Archivo arch = new Archivo("aventureros.in");
+        Archivo arch = new Archivo("TP-aventureros-materos\\resources\\aventureros.in");
         Aventurero aventurero = new Aventurero();
         arch.leerDatos(aventurero);
         arch.cerrar();
@@ -54,39 +16,16 @@ public class Main {
         // Lista para mantener el orden de eliminación
         List<Integer> eliminated = new LinkedList<>();
 
-        // Proceso de eliminación
-        int position = 0;
-        for (int i = 0; i < N - 1; i++) { //Esto es N
-            position = (position + a[i]) % members.size();
-            int removed = members.remove(position); // Es N
-            eliminated.add(removed);
-        }
+        aventurero.crearRondaOias();
+        aventurero.identificadorCebador("TP-aventureros-materos\\resources\\aventureros.out");
 
+        /*
         // Escribo los resultados en el archivo aventureros.out
         PrintWriter pw = new PrintWriter(new FileWriter("aventureros.out"));
         for (int i = 0; i < eliminated.size(); i++) {
             if (i > 0) pw.print(" ");
             pw.print(eliminated.get(i));
         }
-
-        pw.println();
-        pw.println(members.get(0));
-        pw.close();
-/*
-        // Leer el array 'a'
-        int[] a = new int[N - 1];
-        for (int i = 0; i < N - 1; i++) {
-            if (!tokenizer.hasMoreTokens()) {
-                tokenizer = new StringTokenizer(reader.readLine());
-            }
-            a[i] = Integer.parseInt(tokenizer.nextToken());
-        }
-        reader.close();
-
-        
-        List<Integer> rondaOias = crearRondaOias(N);
-        Integer cebador = identificadorCebador("aventureros.out", N, a, rondaOias);
+        */
     }
-*/
-
 }
