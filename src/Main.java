43313cb0583ddb7d590c3ad7947dee943af93df0
LEO <89648679+LeonardoDBarbaro@users.noreply.github.com>
2024-05-26 23:10:17 -0300

    refinada funciones listaOias e identificadorCebador

--- src/Main.java
+++ src/Main.java
@@ -11,26 +11,11 @@ public class Main {
         Archivo arch = new Archivo("./resources/aventureros.in");
         Aventurero aventurero = new Aventurero();
         arch.leerDatos(aventurero);
-        arch.cerrar();
-
-        // Lista para mantener el orden de eliminación
-        List<Integer> eliminated = new LinkedList<>();
-
         aventurero.crearRondaOias();
 
-        Archivo arch_out = new Archivo("./resources/aventureros.out",true);
+        Archivo arch_out = new Archivo("./resources/aventureros.out", true);
 
         aventurero.identificadorCebador(arch_out);
-
         arch_out.cerrar();
-
-        /*
-        // Escribo los resultados en el archivo aventureros.out
-        PrintWriter pw = new PrintWriter(new FileWriter("aventureros.out"));
-        for (int i = 0; i < eliminated.size(); i++) {
-            if (i > 0) pw.print(" ");
-            pw.print(eliminated.get(i));
-        }
-        */
     }
 }
